7fc4857eafbba7d9cfbe7f1fa0155d4eca8abcdf
Dona-j <udyjimmy6@gmail.com>
2022-09-11 12:30:21 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - utput integers
+ *
+ * Description: output the integers of base ten starting from zero
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+
+		if (i < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
