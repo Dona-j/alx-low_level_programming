29bc9634b74c5afc9513db0851b9e91632c6a168
Dona-j <udyjimmy6@gmail.com>
2022-09-14 16:54:46 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,24 +1,21 @@
 #include <unistd.h>
-#include "main.h"
 #include <ctype.h>
+#include "main.h"
 
 /**
- * int _islower(int c) - function that checks for lowercase character.
+ * _islower- Entry poinit
  *
- * Description: using @islower from standard library
+ * @c: integer parameter
+ *
+ * Description: check lowercase character using parameter @c as the alphabet
  *
  * Return: Always (1) or (0) (Success)
  */
 
 int _islower(int c)
 {
-
-	if (c >= 'a' && c <- 'z') || (c >= 'A' && C <= 'Z')
-	{	
+	if (islower(c))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha- Entry point
+ *
+ * @c: integer parameter
+ *
+ * Description: Check alphabet characters
+ *
+ * Return: Always 1 or 0 (success)
+ */
+
+ int _isalpha(int c)
+{
+	if _isalpha(c)
+		return (1);
+	else
+		return (0);
+}
