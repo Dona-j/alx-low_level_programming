fec62b6a1f21f251600ec1a4cbdd5e9e12e8aeb0
Dona-j <udyjimmy6@gmail.com>
2022-09-20 16:20:09 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * str: Entry point parameter
+ *
+ * Return 0 Always.
+ */
+
+
+void print_rev(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+
+	for (--n; n >= 0; --n)
+		_putchar(s[n]);
+
+	_putchar('\n');
