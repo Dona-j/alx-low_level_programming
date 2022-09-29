76901c673966c04c2954a3cc517a9683f9d68c48
Dona-j <udyjimmy6@gmail.com>
2022-09-28 20:57:42 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,16 @@
  * @s: parameter to be reached
  * Return: Always 0.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n')
+		_putchar('\n');
 	}
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1)
+		_puts_recursion(s + 1);
 	}
 
 }
