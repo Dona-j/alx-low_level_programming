1ec06410a4278b694024ace42c6edce04d8564b6
Dona-j <udyjimmy6@gmail.com>
2022-09-28 21:29:47 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _strlen_recursion - function that returns string length
+ *  @s: parameter tobe fulfilled
+ *
+ *  Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+	_putchar('\n');
+}
