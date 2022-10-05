322c2ee516efc118e9008b4763708bb34915e672
Dona-j <udyjimmy6@gmail.com>
2022-10-04 22:40:22 -0700

    README

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: argument passed into main
+ * @argv: pargument passed into main
+ *
+ * Return: Always 0.
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_h
+#define MAIN_H
+
+int _putchar(char c);
+void main(int argc, char *argv[]);
+
+#endif
