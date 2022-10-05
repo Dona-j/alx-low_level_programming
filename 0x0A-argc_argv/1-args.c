3476493bbe30e81ab57788fc12467583d2539e8a
Dona-j <udyjimmy6@gmail.com>
2022-10-05 11:51:24 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  programme that prints the number of arguments passed into it
+ * @argc: argument passed into main
+ * @argv: argument passed into main
+ * Return: Always 0'
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
