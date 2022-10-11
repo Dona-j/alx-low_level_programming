02be8b81550ce962c10c3f541756080c6518372c
Dona-j <udyjimmy6@gmail.com>
2022-10-10 22:02:55 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	char *s;
+
+	s = filename;
+	printf("%s\n", s);
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define filename (__FILE__)
+
+int main(void);
+
+#endif
