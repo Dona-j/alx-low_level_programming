5c3c8fa7c8dde1a592683ba8ae1e897de4a232ff
Dona-j <udyjimmy@gmail.com>
2022-10-20 13:09:48 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a linked list
+ * h: address of a linked list
+ *
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	const list_t *list;
+	list = h;
+
+	while (list != NULL)
+	{
+		if (list->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n" list->len, list->str);
+		}
+		i++;
+	}
+	list->next;
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+Write a function that prints all the elements of a list_t list.
+
+Prototype: size_t print_list(const list_t *h);
+
