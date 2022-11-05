473fe6f26790344f15ba8b1e8ef17706a1b60136
Dona-j <udyjimmy6@gmail.com>
2022-11-04 21:06:55 -0700

    task 1 and 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * create_file - create a file
- * @filename: string name of file
- * @text_content: NULL terminated string to write to the file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, text_len;
@@ -14,9 +16,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
+	/* write */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	/* fd = creat(filename, 0600);*/
 	if (fd == -1)
 		return (-1);
 
--- 0x15-file_io/main/2-main.c
+++ 0x15-file_io/main/2-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code
  * @ac: integer parameter passed into main function
@@ -8,14 +9,14 @@
  */
 int main(int ac, char **av)
 {
-	    int res;
+	int res;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s filename text\n", av[0]);
-		exit(1);
+	dprintf(2, "Usage: %s filename text\n", av[0]);
+	exit(1);
 	}
-	res = create_file(av[1], av[2]);
+	res = append_text_to_file(av[1], av[2]);
 	printf("-> %i)\n", res);
 	return (0);
 }
