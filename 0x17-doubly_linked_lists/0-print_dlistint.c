bfd37060eb709c710a22e2e3feb97e70f77623a4
Dona-j <udyjimmy6@gmail.com>
2022-12-08 20:24:29 -0800

    Add task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,11 @@
-#!/bin/bash
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to head of linked list
+ *
+ * Return: the number of nodes
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
