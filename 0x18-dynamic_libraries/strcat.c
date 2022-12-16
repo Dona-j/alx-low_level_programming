9ba2546a26c8f6356ba83040fde7f8e843283883
Dona-j <udyjimmy6@gmail.com>
2022-12-16 06:45:10 -0800

    Add task 0 file

--- 0x18-dynamic_libraries/strcat.c
+++ 0x18-dynamic_libraries/strcat.c
@@ -1,1 +1,23 @@
+/**
+* _strcat - concatenates two strings
+* @dest: destination string
+* @src: source string
+*
+* Return: char pointer
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
 
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
