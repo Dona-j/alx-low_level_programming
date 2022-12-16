064bc22ee4e226fccb38fd4ed20acbb838d79ee2
Dona-j <udyjimmy6@gmail.com>
2022-12-16 11:11:37 -0800

    Add task 2 files

---
+++ 0x18-dynamic_libraries/cpython.h
@@ -0,0 +1,10 @@
+#ifndef _CPYTHON_H
+#define _CPYTHON_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* _CPYTHON_H */
---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,63 @@
+#include "cpython.h"
+
+/**
+ * add - function that returns sum of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a + b)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - function that returns subtraction of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a - b)
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that returns multiplication of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that returns division of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a / b)
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that returns modulus of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a % b)
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
